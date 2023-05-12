27c8b0bd3cb53759d280616e9c6fbbf7df0c1025
Batjimin <147qqa@gmail.com>
2023-05-12 13:24:29 +0900

    add main.py content

--- source/main.py
+++ source/main.py
@@ -1,2 +1,16 @@
 import pygame as pg
+from . import setup, tools
+from . import Setting as Set
+from . state import Menu, Loading, System
 
+def main():
+    game = tools.Control()
+    state_dict = {
+                  Set.MAIN_MENU: Menu.Menu(),
+                  Set.LOAD_SCREEN: Loading.LoadScreen(),
+                  Set.LEVEL: System.Level(),
+                  Set.GAME_OVER: Loading.GameOver(),
+                  Set.TIME_OUT: Loading.TimeOut()
+                }
+    game.setup_states(state_dict, Set.Menu)
+    game.main()
