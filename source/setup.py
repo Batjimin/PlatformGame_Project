c5b0ca5237bac117a0ab664c7f3d3749713ae473
Batjimin <147qqaa@hufs.ac.kr>
2023-05-13 10:32:18 +0900

    write setup.py

--- source/setup.py
+++ source/setup.py
@@ -1,1 +1,10 @@
 import pygame as pg
+import os
+from . import Setting as Set
+from . import tools
+pg.init()
+pg.event.set_allowed([pg.KEYDOWN, pg.KEYUP, pg.QUIT])
+SCREEN = pg.display.set_mode(Set.SCREEN_SIZE)
+SCREEN_RECT = SCREEN.get_rect()
+
+GFX = tools.load_gfx(os.path.join("resource","Images"))
